3dc1164252cd54ab146e1f9f1a540dba46654df4
Oliver Raileanu <orailean@gmail.com>
2016-10-11 17:30:50 +0200

    First commit

---
+++ src/generated/java/com/thomas_bayer/blz/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://thomas-bayer.com/blz/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.thomas_bayer.blz;
---
+++ src/test/java/automation/NewTest.java
@@ -0,0 +1,29 @@
+package automation;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.thomas_bayer.blz.BLZService;
+import com.thomas_bayer.blz.BLZServicePortType;
+import com.thomas_bayer.blz.DetailsType;
+
+public class NewTest {
+
+	private BLZServicePortType responseSoap12;
+
+	@BeforeTest
+	public void setup() {
+		BLZService blzService = new BLZService();
+		responseSoap12 = blzService.getBLZServiceSOAP12PortHttp();
+	}
+
+	@Test
+	public void DeutscheBank() {
+		DetailsType bankDetails = responseSoap12.getBank("70070024");
+		Assert.assertTrue(bankDetails.getBezeichnung().contains("Deutsche Bank"));
+		Assert.assertEquals(bankDetails.getOrt(), "München");
+		Assert.assertEquals(bankDetails.getPlz(), "80271");
+	}
+
+}
